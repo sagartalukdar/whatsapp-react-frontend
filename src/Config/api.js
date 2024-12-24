9892091a18142d0b640e47d6acc6da4ee4bf1dce
sagartalukdar <121626196+sagartalukdar@users.noreply.github.com>
2024-12-24 20:35:41 +0530

    Update api.js

--- src/Config/api.js
+++ src/Config/api.js
@@ -2,4 +2,4 @@
 
 // export const base_url='http://localhost:8080'
 
-export const base_url='whatsapp-springboot-server-production.up.railway.app'
+export const base_url='https://whatsapp-springboot-server-production.up.railway.app'
